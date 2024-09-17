c1a6e4f030dab813cf59279d20915d3604550800
정용우 <jyw1341@gmail.com>
2024-09-17 14:50:48 +0900

    OrderController 주문 추가 API 반환값 수정

--- src/main/java/com/rgt/assignment/controller/OrderController.java
+++ src/main/java/com/rgt/assignment/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.rgt.assignment.controller;
 import com.rgt.assignment.argumentsresolver.Login;
 import com.rgt.assignment.dto.LoginInfo;
 import com.rgt.assignment.dto.OrderRequest;
+import com.rgt.assignment.dto.OrderResponse;
 import com.rgt.assignment.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,7 +21,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order")
-    public void addOrder(@RequestBody List<OrderRequest> orderRequests, @Login LoginInfo loginInfo) {
-        orderService.addOrder(orderRequests, loginInfo);
+    public OrderResponse addOrder(@RequestBody List<OrderRequest> orderRequests, @Login LoginInfo loginInfo) {
+        return new OrderResponse(orderService.addOrder(orderRequests, loginInfo));
     }
 }
