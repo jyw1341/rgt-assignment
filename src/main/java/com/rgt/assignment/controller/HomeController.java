42aafc9f5330491a6abd36f65fb5a350036b8630
정용우 <jyw1341@gmail.com>
2024-09-17 15:22:35 +0900

    HomeController 인사말 수정

--- src/main/java/com/rgt/assignment/controller/HomeController.java
+++ src/main/java/com/rgt/assignment/controller/HomeController.java
@@ -1,18 +1,13 @@
 package com.rgt.assignment.controller;
 
-import com.rgt.assignment.domain.Member;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = "loginMember", required = false) Member member) {
-        if(member == null) {
-            return "로그인이 필요합니다";
-        }
-        return member.getName();
+    public String home() {
+        return "[원티드, 정용우, 백엔드] 과제제출";
     }
 }
