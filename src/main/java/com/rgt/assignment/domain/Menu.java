9e4cf4ccab146ad5c4338245ad3f6fbc4c87a335
정용우 <jyw1341@gmail.com>
2024-09-17 10:16:37 +0900

    Menu 엔티티 수정
    생성자 추가
    Restaurant 엔티티 연관관계 설정

--- src/main/java/com/rgt/assignment/domain/Menu.java
+++ src/main/java/com/rgt/assignment/domain/Menu.java
@@ -1,12 +1,14 @@
 package com.rgt.assignment.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Menu {
 
     @Id
@@ -14,9 +16,20 @@ public class Menu {
     @Column(name = "menu_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
     private String name;
 
     private Integer price;
 
     private String description;
+
+    public Menu(Restaurant restaurant, String name, Integer price, String description) {
+        this.restaurant = restaurant;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
 }
