9fe1b47dcd2722c9a4d5c52f6c054d4cc5dfed23
정용우 <jyw1341@gmail.com>
2024-09-17 10:19:53 +0900

    Restaurant 엔티티 생성자 추가

--- src/main/java/com/rgt/assignment/domain/Restaurant.java
+++ src/main/java/com/rgt/assignment/domain/Restaurant.java
@@ -1,20 +1,27 @@
 package com.rgt.assignment.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Restaurant {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "restaurant_id")
     private Long id;
 
     private String name;
 
     private Integer tableQuantity;
+
+    public Restaurant(Long id, String name, Integer tableQuantity) {
+        this.id = id;
+        this.name = name;
+        this.tableQuantity = tableQuantity;
+    }
 }
