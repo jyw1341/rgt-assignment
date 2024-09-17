99e19604f3b0a7c1ffc166ed0c0aa96491ad9ab3
정용우 <jyw1341@gmail.com>
2024-09-17 10:15:03 +0900

    Cart 엔티티 기본 생성자 추가

--- src/main/java/com/rgt/assignment/domain/Cart.java
+++ src/main/java/com/rgt/assignment/domain/Cart.java
@@ -1,12 +1,14 @@
 package com.rgt.assignment.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Cart {
 
     @Id
