e96a07ce95f754c9e7ea17e6692efe4efadeaacf
정용우 <jyw1341@gmail.com>
2024-09-17 11:24:13 +0900

    세션 관련 상수 수정

--- src/main/java/com/rgt/assignment/constant/SessionConstant.java
+++ src/main/java/com/rgt/assignment/constant/SessionConstant.java
@@ -1,8 +1,5 @@
 package com.rgt.assignment.constant;
 
 public interface SessionConstant {
-    public static String MEMBER_ID = "memberId";
-    public static String MEMBER_NAME = "memberName";
-    public static String RESTAURANT_ID = "restaurantId";
-    public static String TABLE_NUMBER = "tableNumber";
+    public static final String LOGIN_INFO = "loginMember";
 }
