8d56ba6b912ba3812d87e5ff5eb0bea745264698
정용우 <jyw1341@gmail.com>
2024-09-17 11:24:56 +0900

    WebConfig LoginArgumentResolver 추가

--- src/main/java/com/rgt/assignment/config/WebConfig.java
+++ src/main/java/com/rgt/assignment/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.rgt.assignment.config;
 
+import com.rgt.assignment.argumentsresolver.LoginArgumentResolver;
 import com.rgt.assignment.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +18,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/", "/login", "/logout", "/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginArgumentResolver());
     }
 }
