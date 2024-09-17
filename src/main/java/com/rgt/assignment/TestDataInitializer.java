710fd35b202e4cc8f7ac8df5be4f81c712b68315
정용우 <jyw1341@gmail.com>
2024-09-17 14:51:54 +0900

    테스트용 데이터 추가하는 클래스 구현

--- src/main/java/com/rgt/assignment/TestDataInitializer.java
+++ src/main/java/com/rgt/assignment/TestDataInitializer.java
@@ -1,7 +1,11 @@
 package com.rgt.assignment;
 
 import com.rgt.assignment.domain.Member;
+import com.rgt.assignment.domain.Menu;
+import com.rgt.assignment.domain.Restaurant;
 import com.rgt.assignment.repository.MemberRepository;
+import com.rgt.assignment.repository.MenuRepository;
+import com.rgt.assignment.repository.RestaurantRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,10 +15,20 @@ import org.springframework.stereotype.Component;
 public class TestDataInitializer {
 
     private final MemberRepository memberRepository;
+    private final MenuRepository menuRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @PostConstruct
     public void init() {
-        Member member = new Member("test", "test@gmail.com", "1234");
-        memberRepository.save(member);
+        memberRepository.save(new Member("tester1", "test1@gmail.com", "1234"));
+        memberRepository.save(new Member("tester2", "test2@gmail.com", "1234"));
+        memberRepository.save(new Member("tester3", "test3@gmail.com", "1234"));
+
+        Restaurant restaurant = new Restaurant(123L,"큭큭피자", 10);
+        restaurantRepository.save(restaurant);
+
+        menuRepository.save(new Menu(restaurant, "감자밭 피자", 25000, "감자"));
+        menuRepository.save(new Menu(restaurant, "통통 새우 피자", 25000, "새우"));
+        menuRepository.save(new Menu(restaurant,"베이컨 피자", 25000, "베이컨"));
     }
 }
